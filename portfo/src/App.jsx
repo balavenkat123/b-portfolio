a25dc5d4bdca8073a82016e3129a08fa2efd45bf
balavenkat123 <balavenkat17902@gmail.com>
2023-07-14 10:56:52 +0530

    setup

---
+++ portfo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+function App() {
+ 
+
+  return (
+    <h1 className="text-7xl font-bold underline font-signature">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
