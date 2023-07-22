c0a4f47e997f8a6923b944af81af6650b431e8b6
balavenkat123 <balavenkat17902@gmail.com>
2023-07-22 21:53:39 +0530

    portfoliossecupdate

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,44 +5,44 @@ import navbar from "../assets/portfolio/navbar.jpg"
 import reactParallax from "../assets/portfolio/reactParallax.jpg"
 import reactPortfolio2 from "../assets/portfolio/reactPortfolio2.jpg"
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg"
-import reactWeather from "../assets/portfolio/reactWeather.jpg"
+// import reactWeather from "../assets/portfolio/reactWeather.jpg"
 
 const Portfolio = () => {
 
-  const portfolios = [
-    {
-      id:1,
-      src: arrayDestruct
-      // link: "https://b-shopzz.netlify.app"
+  // const portfolios = [
+  //   {
+  //     id:1,
+  //     src: arrayDestruct
+  //     // link: "https://b-shopzz.netlify.app"
 
-    },
-    {
-      id:2,
-      src: installNode
+  //   },
+  //   {
+  //     id:2,
+  //     src: installNode
       
 
-    },
-    {
-      id:3,
-      src: navbar
+  //   },
+  //   {
+  //     id:3,
+  //     src: navbar
 
-    },
-    {
-      id:4,
-      src: reactParallax
+  //   },
+  //   {
+  //     id:4,
+  //     src: reactParallax
 
-    },
-    {
-      id:5,
-      src: reactPortfolio2
+  //   },
+  //   {
+  //     id:5,
+  //     src: reactPortfolio2
 
-    },
-    {
-      id:6,
-      src: reactSmooth
+  //   },
+  //   {
+  //     id:6,
+  //     src: reactSmooth
 
-    },
-  ]
+  //   },
+  // ];
 
   return (
     <div className="h-screen w-full bg-gradient-to-b from-black via-blue-950 to-blue-900 text-white md:h-screen">
@@ -57,7 +57,42 @@ const Portfolio = () => {
             
 
           })} */}
-          <div  className='shadow-md shadow-purple-800 rounded-lg'>
+          <div   className='shadow-md shadow-purple-800 rounded-lg'>
+              <img  src={arrayDestruct} alt="" className='hover:scale-105 duration-200 rounded-md'/>
+              <div className='flex items-center justify-center '>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold  '>Demo</button>
+                <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold'>Code</button>
+              </div>
+          </div>
+          <div   className='shadow-md shadow-purple-800 rounded-lg'>
+              <img  src={arrayDestruct} alt="" className='hover:scale-105 duration-200 rounded-md'/>
+              <div className='flex items-center justify-center '>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold  '>Demo</button>
+                <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold'>Code</button>
+              </div>
+          </div>
+          <div   className='shadow-md shadow-purple-800 rounded-lg'>
+              <img  src={arrayDestruct} alt="" className='hover:scale-105 duration-200 rounded-md'/>
+              <div className='flex items-center justify-center '>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold  '>Demo</button>
+                <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold'>Code</button>
+              </div>
+          </div>
+          <div   className='shadow-md shadow-purple-800 rounded-lg'>
+              <img  src={arrayDestruct} alt="" className='hover:scale-105 duration-200 rounded-md'/>
+              <div className='flex items-center justify-center '>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold  '>Demo</button>
+                <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold'>Code</button>
+              </div>
+          </div>
+          <div   className='shadow-md shadow-purple-800 rounded-lg'>
+              <img  src={arrayDestruct} alt="" className='hover:scale-105 duration-200 rounded-md'/>
+              <div className='flex items-center justify-center '>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold  '>Demo</button>
+                <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold'>Code</button>
+              </div>
+          </div>
+          <div   className='shadow-md shadow-purple-800 rounded-lg'>
               <img  src={arrayDestruct} alt="" className='hover:scale-105 duration-200 rounded-md'/>
               <div className='flex items-center justify-center '>
                 <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150 font-bold  '>Demo</button>
