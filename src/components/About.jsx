f686c1965ff56b6d5c22627080273cb46e41d30d
balavenkat123 <balavenkat17902@gmail.com>
2023-07-22 20:03:06 +0530

    aboutsecchanges

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,25 +4,27 @@ import React from "react";
 const About = () => {
   return (
     <section className="h-screen w-full bg-gradient-to-b from-blue-950 via-blue-900 to-black text-white md:h-screen" id="about">
-      <div className="container mx-auto grid  items-center justify-center ">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
         <div className="about-info">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2 ">
             About Me
           </h2>
 
-          <p className="pb-5 px-10">
+          <p className="pb-5 px-10 text-xl">
             Hi, My Name Is Bala venkat everyone calls me Bala. I am a
             Frontend Developer. I build beautifull websites with React and
             Tailwind CSS.
           </p>
-          <p className="pb-5 px-10">
+          <br />
+          <p className="pb-5 px-10 text-xl">
             I am proficient in Frontend skills like React.js, Redux, Redux Tool
             Kit, Axios, Tailwind CSS, SaSS, Css3 and many more.
           </p>
+          <br />
 
-          <p className="pb-5 px-10">In backend I know Node.js and firebase</p>
+          <p className="pb-5 px-10 text-xl">In backend I know Node.js and firebase</p>
 
-          <p className="pb-5 px-10">
+          <p className="pb-5 px-10 text-xl">
             In my spare time I create YouTube videos and write blogs on my Blog.
             Where I talk about programming theory and build various projects.
           </p>
